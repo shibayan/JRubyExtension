a19682c3432354959e04e7a95948238efb95d131
shibayan <shiba-yan@outlook.com>
2016-02-15 23:50:30 +0900

    Change JRuby version to 9.0.5.0

--- content/install.cmd
+++ content/install.cmd
@@ -3,7 +3,8 @@
 :: Setup
 :: ----------------
 
-set JRUBY_VERSION=9.0.4.0
+set JRUBY_VERSION=9.0.5.0
+
 set JRUBY_HOME=%HOME%\SiteExtensions\jruby\jruby-%JRUBY_VERSION%
 set JRUBY_EXE=%JRUBY_HOME%\bin\jruby.exe
 set JRUBY_GEM_CMD=%JRUBY_HOME%\bin\gem
